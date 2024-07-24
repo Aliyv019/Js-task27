a37d12086bd5f3625dd61139bbcd859936eb6529
Aliyv019 <tahiraliyev2006@gmail.com>
2024-07-24 20:21:36 +0400

    some minor changes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const api_key="5c1e801a98de55b805c069472e3bdc3a"
-const city=document.querySelector('input').value
 
-async function fetchData(city){
-    city=document.querySelector('input').value
+async function fetchData(){
+    const city=document.querySelector('input').value
     try{
         const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
         const data=await response.json()
@@ -12,7 +11,7 @@ async function fetchData(city){
         document.querySelectorAll('span')[1].textContent=`${data.main.temp}°C`
     }
     catch (error){
-        console.log(error);
+        console.log(error.message);
     }
 }
 document.querySelector('button').addEventListener('click',fetchData)
